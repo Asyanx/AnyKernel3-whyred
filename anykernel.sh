eb7b213d957f4a39ddfea0e8c0c0b07c40b68884
Alixxxxxxxixixi <76146196+Asyanx@users.noreply.github.com>
2026-09-03 01:05:47 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string= Kernel by Asyanx @ Telegram
+kernel.string= join the support group......
 do.devicecheck=1
 do.modules=0
 do.systemless=1
